4dbcf9428829bd27aaaed093c48313ebd115ad7d
xksdn <xksdn7623@gmail.com>
2024-07-23 18:08:51 +0900

    first

---
+++ src/main/java/UtoPlan/UtoPlan/UtoPlanApplication.java
@@ -0,0 +1,13 @@
+package UtoPlan.UtoPlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtoPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtoPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UtoPlan/UtoPlan/UtoPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package UtoPlan.UtoPlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtoPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
