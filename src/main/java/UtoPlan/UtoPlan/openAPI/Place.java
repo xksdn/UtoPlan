dab65eeb30011d2bc9dd92562c27893e23ce2b6a
xksdn <xksdn7623@gmail.com>
2024-09-25 15:14:47 +0900

    Places api v1

---
+++ src/main/java/UtoPlan/UtoPlan/openAPI/Place.java
@@ -0,0 +1,20 @@
+package UtoPlan.UtoPlan.openAPI;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// google places api에서 받아온 정보를 저장해두는 객체
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Place {
+    private String name;
+    private double lat;
+    private double lng;
+    private String address;
+    private double rating;
+}
