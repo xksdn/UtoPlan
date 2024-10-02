62d928f2d2e34a90cc73a080f0e1f712097602e9
asdf <aendckr@gmail.com>
2024-10-02 14:18:10 +0900

    sign up fin

--- src/main/java/UtoPlan/UtoPlan/DB/UserService.java
+++ src/main/java/UtoPlan/UtoPlan/DB/UserService.java
@@ -14,16 +14,20 @@ public class UserService {
     // 회원가입을 처리하는 메서드
     public UserEntity registerUser(UserEntity user) {
         // 중복 사용자 검사 (ID를 기준으로)
-        if (getUserById(user.getUser_id()).isPresent()) {
-            throw new IllegalArgumentException("ID가 이미 존재하는 사용자입니다.");
-        }
+        //if (getUserByUserId(user.getUser_id()).isPresent()) {
+        //    throw new IllegalArgumentException("ID가 이미 존재하는 사용자입니다.");
+        //}
 
         // 사용자 저장
         return userRepository.save(user);
     }
+    // user_id로 조회
+    //public Optional<UserEntity> getUserByUserId(String user_id) {
+        //return userRepository.findByUserId(user_id);
+    //}
 
-    // 사용자 조회
-    public Optional<UserEntity> getUserById(String id) {
+    // num으로 조회
+    public Optional<UserEntity> getUserById(Long id) {
         return userRepository.findById(id);
     }
 }
